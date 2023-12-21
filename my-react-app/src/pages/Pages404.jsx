54335b559eb1ab01abfae3cae8702c0c89f67e3a
RubenKraan <125913576+RubenKraan@users.noreply.github.com>
2023-12-21 11:37:37 +0100

    page404 created

--- my-react-app/src/pages/Pages404.jsx
+++ my-react-app/src/pages/Pages404.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+import NotFound404 from "../components/NotFound404";
+
+const Page404 = () => {
+    return(
+        <div className="page-404">
+            <NotFound404 />
+        </div>
+    );
+    
+};
+
+export default Page404;
