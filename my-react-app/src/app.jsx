cdd6eee8dc0f7b1aa92e7dd5f824207d6bc8245d
RubenKraan <125913576+RubenKraan@users.noreply.github.com>
2024-01-09 17:50:05 +0100

    Update app.jsx

--- my-react-app/src/app.jsx
+++ my-react-app/src/app.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DataDone } from "./components/DataContext";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import KasaLocation from "./pages/KasaLocation.js";
 import Page404 from "./pages/Page404";
+import { DataDone } from "./components/DataContext";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 
 const App = () => {
-    let basename = "/";
-    if (window.location.hostname === "RubenKraan.github.io") {
-      basename = "/projet-6oc";
-    }
-    return (
-        <BrowserRouter basename={basename}> 
-            <DataDone>
-                <Routes>
-                </Routes>
-            </DataDone> 
-        </BrowserRouter>
-    );
+  let basename = "/";
+  if (window.location.hostname === "youcefknl.github.io") {
+    basename = "/OC_Projet6_Kasa";
+  }
+  return (
+    <BrowserRouter basename={basename}>
+      <DataDone>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/location/:id" element={<KasaLocation />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+        <Footer />
+      </DataDone>
+    </BrowserRouter>
+  );
 };
+
+export default App;
